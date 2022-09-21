fd30ae6c11569b10a45532c75338e5ee40cd5cba
andr777 <andr.root@gmail.com>
2022-09-21 05:46:49 +0300

    1

---
+++ src/main/java/pro/sky/HomeWork15CollectionsAndSets/HomeWork15CollectionsAndSetsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.HomeWork15CollectionsAndSets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork15CollectionsAndSetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork15CollectionsAndSetsApplication.class, args);
+	}
+
+}
